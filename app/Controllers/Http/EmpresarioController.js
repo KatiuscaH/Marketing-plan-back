6d588a55527341b295ff8935424b7dbb7aef5aa1
Wilmer Llamas <wilmer199609@gmail.com>
2018-10-10 15:22:57 -0500

    Empresario

---
+++ app/Controllers/Http/EmpresarioController.js
@@ -0,0 +1,34 @@
+'use strict'
+
+class EmpresarioController {
+    async index() {
+        return await User
+            .query()
+            .where({ rol: 2 })
+    }
+    async show({ params }) {
+        return await User.find(params.id)
+    }
+    async store({ request }) {
+        const userData = request.only(['name', 'lastname', 'password', 'email', 'periodo', 'year'])
+        const rulesUser = {
+            name: 'required|string',
+            lastname: 'required|string',
+            email: 'required|email|unique:users,email',
+            password: 'required',
+            periodo: 'required|number',
+            year: 'required|number'
+        }
+        const userValidation = await validate(userData, rulesUser)
+        if (userValidation.fails()) {
+            return { error: userValidation._errorMessages }
+        }
+        const user = new User()
+        user.fill(userData)
+        user.rol = 1;
+        await user.save()
+        return user
+    }
+}
+
+module.exports = EmpresarioController
